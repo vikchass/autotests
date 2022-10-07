e3e9a2664eed79f4967d65b071eed7346b0b4869
victoryrodionova <rodionova.victory@yandex.ru>
2022-10-07 13:45:56 +0300

    homework from previous module

--- python.py
+++ python.py
@@ -512,44 +512,59 @@
 
 #  ОЖИДАНИЕ (хорошее)- явное2
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-import time
-import math
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium import webdriver
+# import time
+# import math
+#
+#
+# def calc(x):
+#     return str(math.log(abs(12 * math.sin(int(x)))))
+#
+# link = "http://suninjuly.github.io/explicit_wait2.html"
+#
+# try:
+#     browser = webdriver.Chrome()
+#     browser.get(link)
+#
+#     WebDriverWait(browser, 12).until(
+#         EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+#     )
+#
+#     button = browser.find_element(By.ID, "book")
+#     button.click()
+#
+#     x_el= browser.find_element(By.CSS_SELECTOR, "#input_value")
+#     x = x_el.text
+#     y = calc(x)
+#
+#     x = browser.find_element(By.CSS_SELECTOR, "#answer")
+#     x.send_keys(y)
+#
+#     button = browser.find_element(By.ID, "solve")
+#     button.click()
+#
+# finally:
+#     time.sleep(10)
+#     browser.quit()
+#
+#
+#
+# def test_input_text(expected_result, actual_result):
 
 
-def calc(x):
-    return str(math.log(abs(12 * math.sin(int(x)))))
-
-link = "http://suninjuly.github.io/explicit_wait2.html"
-
-try:
-    browser = webdriver.Chrome()
-    browser.get(link)
-
-    WebDriverWait(browser, 12).until(
-        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
-    )
-
-    button = browser.find_element(By.ID, "book")
-    button.click()
-
-    x_el= browser.find_element(By.CSS_SELECTOR, "#input_value")
-    x = x_el.text
-    y = calc(x)
-
-    x = browser.find_element(By.CSS_SELECTOR, "#answer")
-    x.send_keys(y)
-
-    button = browser.find_element(By.ID, "solve")
-    button.click()
-
-finally:
-    time.sleep(10)
-    browser.quit()
 
 
 
 
+
+s = 'My Name is Julia'
+
+if 'Name' in s:
+    print('Substring found')
+
+index = s.find('Name')
+if index != -1:
+    print(f'Substring found at index {index}')
