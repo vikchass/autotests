eb9207d909c7b4fc231422c5dc15a95c6f0e2eb6
victoryrodionova <rodionova.victory@yandex.ru>
2022-10-07 18:22:18 +0300

    test web code

--- test_web.py
+++ test_web.py
@@ -8,3 +8,39 @@
 # f"expected '{substring}' to be substring of '{full_string}'"
 
 
+# def test_abs1():
+#     assert abs(-42) == 42, "Should be absolute value of a number"
+#
+# if __name__ == "__main__":
+#     test_abs1()
+#     print("All tests passed!")
+
+
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://suninjuly.github.io/registration2.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[1]/input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[2]/input")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.XPATH, "/ html / body / div / form / div[1] / div[3] / input")
+    input3.send_keys("ggg@mail.ru")
+    input4 = browser.find_element(By.XPATH, "/html/body/div/form/div[2]/div[1]/input")
+    input4.send_keys("77777777777")
+    input5 = browser.find_element(By.XPATH, "/html/body/div/form/div[2]/div[2]/input")
+    input5.send_keys("Russia")
+    button = browser.find_element(By.XPATH, "/html/body/div/form/button")
+    button.click()
+
+finally:
+    time.sleep(10)
+    browser.quit()
