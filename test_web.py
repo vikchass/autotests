3f076d833af478a567b4a9f9e08c65cd0b9769a3
victoryrodionova <rodionova.victory@yandex.ru>
2022-10-07 13:46:16 +0300

    test web code

---
+++ test_web.py
@@ -0,0 +1,10 @@
+# def test_input_text(expected_result, actual_result):
+#    assert expected_result == actual_result, \
+#     f"expected {expected_result}, got {actual_result}"
+
+
+# def test_substring(full_string, substring):
+#     assert substring in full_string, \
+# f"expected '{substring}' to be substring of '{full_string}'"
+
+
